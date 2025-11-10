42c9ceba968d1463f2134f299192c79ebedb5137
Gabriel Araújo <146504189+Resouce13@users.noreply.github.com>
2025-11-10 10:54:42 -0300

    Update QUICKSORT.C

--- src/operacoes/QUICKSORT.C
+++ src/operacoes/QUICKSORT.C
@@ -30,3 +30,25 @@ void quicksort(Cliente vet[],int low,int high){
     }
 }
 
+static int nome(Cliente vet[], int low, int high){
+    Cliente pi = vet[high];
+    int i = low;
+
+    for(int j = low; j < high; j++){
+        if(strcmp(vet[j].nome, pi.nome) <= 0){
+            trocar(&vet[i], &vet[j]);
+            i++;
+        }
+    }
+
+    trocar(&vet[i], &vet[high]);
+    return 1;
+}
+
+void quicksort(Cliente vet[],int low,int high){
+    if(low < high){ 
+        int pi=nome(vet, low, high); 
+        quicksort(vet, low, pi - 1); 
+        quicksort(vet, pi + 1, high); 
+    }
+}
